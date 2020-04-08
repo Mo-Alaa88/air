ddb53686437c372d34b7950d0b373bc8762c3c49
mo-alaa88 <mohammed.alaa.al.din@gmail.com>
2020-04-08 23:53:18 +0200

    Creat css file

--- component/nav.js
+++ component/nav.js
@@ -2,13 +2,15 @@ import Link from 'next/link'
 
 const Nav =()=>(
     <nav>
-        <img src='../images/logo.webp' alt= 'Logo'/>
-        <Link href='../'>
-            <a>home</a>
-        </Link>
-        <Link href='../sharp'>
-            <a>sharp</a>
-        </Link>
+            <img src='https://www.elarabygroup.com/skin/frontend/smartwave/elaraby/images/elaraby-group-logo.png' alt='Logo' className='col-m-2'/>
+        <div>
+            <Link href='../'>
+                <a className='col-m-2'>home</a>
+            </Link>
+            <Link href='../sharp'>
+                <a className='col-m-2'>sharp</a>
+            </Link>
+        </div>
     </nav>
 )
 export default Nav
